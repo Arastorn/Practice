8c5a4756fc0ca9093005e1a819201a27ca58a445
Arastorn <antoine.bourgeois1996@gmail.com>
2021-02-02 15:59:36 +0100

    setup code analysers

--- src/Practice.Api/Controllers/WeatherForecastController.cs
+++ src/Practice.Api/Controllers/WeatherForecastController.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Practice.Api.Models;
 
 namespace Practice.Api.Controllers
 {
@@ -16,11 +16,11 @@ namespace Practice.Api.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastController> logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-            _logger = logger;
+            this.logger = logger;
         }
 
         [HttpGet]
@@ -31,7 +31,7 @@ namespace Practice.Api.Controllers
                 {
                     Date = DateTime.Now.AddDays(index),
                     TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
+                    Summary = Summaries[rng.Next(Summaries.Length)],
                 })
                 .ToArray();
         }
